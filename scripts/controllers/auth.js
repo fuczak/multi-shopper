bc0a549c7a0add715c2e6795550e43cf5b6be834
Damian Prus <prus.damian@gmail.com>
2015-03-30 23:41:57 +0200

    added toaster notifications

--- scripts/controllers/auth.js
+++ scripts/controllers/auth.js
@@ -1,9 +1,12 @@
 'use strict';
 
-app.controller('AuthCtrl', ['$scope', 'Auth', function($scope, Auth) {
+app.controller('AuthCtrl', ['$scope', 'Auth', 'toaster', '$location', function($scope, Auth, toaster, $location) {
 	$scope.register = function(user) {
 		Auth.register(user).then(function(data) {
-			alert(JSON.stringify(data))
+			console.log(data)
+		 	$scope.user = '';
+		 	$location.path('/');
+			toaster.pop('success', 'Thank you for signing up!');
 		});
 	}
 }]);
